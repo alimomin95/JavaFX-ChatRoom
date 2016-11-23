abf2339fba739640c27a8585509bc445175b18d4
Quinn Z <Quinn Z>
2016-11-22 19:32:20 -0600

    start coding clientside networking code

--- assignment7/Client.java
+++ assignment7/Client.java
@@ -1,32 +1,63 @@
 package assignment7;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
+
+
+//client is initialized with an output text field and an input field
+//after initialize, you call setUpNetworking with an address and a port
 public class Client {
-	BufferedReader reader;
-	PrintWriter writer;
+	private Socket sock;
+    private BufferedReader reader;
+	private PrintWriter writer;
+	private TextField incoming;
+	private TextArea outgoing;
+	
+	public Client(TextField reader, TextArea writer){
+		this.incoming = reader;
+		this.outgoing = writer;
+	}
 	
 	
-	
-	private void setUpNetworking(String address, int port) throws Exception {
-		Socket sock = new Socket(address, port);
+	public void setUpNetworking(String address, int port) throws Exception {
+		sock = new Socket(address, port);
 		InputStreamReader streamReader = new InputStreamReader(sock.getInputStream());
 		reader = new BufferedReader(streamReader);
 		writer = new PrintWriter(sock.getOutputStream());
-		Thread readerThread = new Thread(new IncomingReader());
+		Thread readerThread = new Thread(new incomingReader());
 		readerThread.start();
+	}
+	
+	public void closeNetworking() throws IOException{
+		sock.close();
+	}
+
+	public void sendMessage(String message){
+        incoming.setText(incoming.getText() + message + "\n");
+	}
+	
+	
+	
+	private class incomingReader implements Runnable {
+		@Override
+		public void run() {
+			String message = null;
+			while(true){
+				try{
+					message = reader.readLine();
+			        incoming.setText(incoming.getText() + message + "\n");
+				} catch (IOException e) {
+				
+				}				
+			}
+		}
 	}
 }
 
-class incomingReader implements Runnable {
-	@Override
-	public void run() {
-		String message;
-		while((message = reader.readLine()) != null) {
-			incoming.append(message + "\n");
-		}
-	}
